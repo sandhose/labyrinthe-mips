cd487616fec09c7dcdb10bee26784c177bd19109
nargy <nicolas.argyriou95@gmail.com>
2016-11-19 16:26:17 +0100

    Réécriture de print_table 'u_u

--- projet.s
+++ projet.s
@@ -1,31 +1,31 @@
 # projet.s
 
 .data
-seed: .word 0xdeadbeef, 0x13371337
-max_random_float: .float 2147483647
-str1: .asciiz "Random number: "
-TableWidth: .asciiz "Table width: "
-StoredAt: .asciiz "Stored at: "
-RetChar: .asciiz "\n"
+seed: 
+	.word 0xdeadbeef, 0x13371337
+max_random_float: 
+	.float 2147483647
+str1: 
+	.asciiz "Random number: "
+TableWidth: 
+	.asciiz "Table width: "
+StoredAt: 
+	.asciiz "Stored at: "
+NewLine: 
+	.asciiz "\n"
 .text
 .globl __start
 
 # Entry point
 __start:
-	li	$v0	4
-	la	$a0	str1
-	syscall
-
-	li	$a0	0
-	li	$a1	101
-	jal	RandomBetween
-	move	$a0	$v0
-	li	$v0	1
-	syscall
-
-
+	li	$a0	32
+	li	$a1	0
 	jal	CreateTable
 
+	move	$a0	$v0
+	li	$a1	32
+	jal	PrintTable
+	
 	j	exit
 
 # Returns a random integer included in [$a0,$a1[
@@ -92,108 +92,68 @@ random_generator:
 #				$a1: 0: Sorted in ascending order,
 #					 1: Sorted in descending order,
 #					 2: Scrambled
-# Returns : Adress of the first int in the table
+# Returns : Adress of the first int in the table in $v0
 CreateTable:
 	move	$t0	$a0
-	mul	$a0	$a0	4	# taille en octets
-	li	$v0	9
+	mul	$a0	$a0	4	#width in bytes
+	li	$v0	9		#malloc of size 4*n
 	syscall
-	move	$t1	$v0
-	beq	$a1	0	Increasing
-	beq	$a1	1	Decreasing
-	beq	$a1	2	Random
-
-Increasing:
-	li	$t2	0	#counter
-	mul	$t3	$t0	4
+	move	$t1	$v0	#t1: address
+	li	$t2	0	#t2: offset
+	li	$t3	15 	#t3: stored constant
 	__Loop_Increasing:
-		beq	$t2	$t3	__JR
-		addu	$t4	$t1	$t2
-		sw	$t2	0($t4)
+		beq	$t2	$a0	__JR
+		addu	$t4	$t1	$t2 #t4: adress + offset
+		sw	$t3	0($t4)
 		addu	$t2	$t2	4
 		j	__Loop_Increasing
 
-Decreasing:
-	li	$t2	0	#counter
-	mul	$t3	$t0	4	# max n
-	__Loop_Decreasing:
-		beq	$t2	$t3	__JR
-		addu	$t4	$t1	$t2
-		subu	$t5	$t3	$t2
-		sw	$t5	0($t4)
-		addu	$t2	$t2	4
-		j	__Loop_Decreasing
-Random:
-	li	$t2	0	#counter
-	mul	$t3	$t0	4	# max n
-	__Loop_Random:
-		beq	$t2	$t3	__JR
-		addu	$t4	$t1	$t2
-
-		move	$t6	$a0	#save a0
-		li	$v0	41
-		syscall
-
-
-		move	$t5	$a0
-		move	$a0	$t6
-		move	$v0	$t1
-
-		sw	$t5	0($t4)
-		addu	$t2	$t2	4
-		j	__Loop_Random
-
 # Function PrintTable
-# Parameters : $a0: width of the table (as in how many integers)
+# Parameters : 	$a0: adress of the first integer in the table
+#		$a1: width of the table (as in how many integers)
 # Pre-conditions: $a0 >=0
 # Returns: -
 PrintTable:
 #Prologue:
-	subu	$sp	$sp	24
-	sw	$s0	20($sp)
-	sw	$s1	16($sp)
-	sw	$s2	12($sp)
-	sw	$a0	8($sp)
-	sw	$a1	4($sp)
+	subu	$sp	$sp	4
 	sw	$ra	0($sp)
 #Body:
+	move	$t0	$a0 #t0: adress of the first integer in the table
+	move	$t1	$a1 #t1: width of the table (as in how many integers)
+	#printing "Table width: X"
 	la	$a0	TableWidth
-	li	$v0	4
-	syscall
-	lw	$a0	8($sp)
-	jal	PrintInt
+	li	$v0	4 
+	syscall 
+	move	$a0	$t1
+	jal	PrintInt 
+	
+	#printing "stored at X"
 	la	$a0	StoredAt
 	li	$v0	4
-	syscall
-	lw	$a0	4($sp)
+	syscall 
+	move	$a0	$t0
 	jal	PrintInt
-
-	lw	$a0	8($sp)
-	lw	$a1	4($sp)
-
-	li	$s0	4
-	mul	$s2	$a0	$s0 #$a0: Bytes needed to store the table
-	li	$s1	0 #s1: offset
+	
+	#Printing table's content
+	li	$t2	4
+	mul	$t1	$t1	$t2 	#$t1: Bytes needed to store the table
+	li	$t3	0 		#$t3: offset
+	#add	$t4	$t1	$t0 	#$t4: end adress of the table
 	__Loop_PrintTable:
-		bge	$s1	$s2	Fin__Loop_PrintTable
-		lw	$a1	4($sp)
-		add	$t0	$a1	$s1	#Adress of the first int of the table + offset
-		lw	$a0	0($t0)
+		bge	$t3	$t1	Fin__Loop_PrintTable
+		add	$t4	$t0	$t3	#Adress of the first int of the table + offset
+		lw	$a0	0($t4)
 		jal	PrintInt
-		addi	$s1	$s1	4	#increment	offset
+		addi	$t3	$t3	4	#increment offset
 		j __Loop_PrintTable
 Fin__Loop_PrintTable:
-	la	$a0	RetChar
+	#print newline
+	la	$a0	NewLine
 	li	$v0	4
 	syscall
 #Epilogue:
-	lw	$s0	20($sp)
-	lw	$s1	16($sp)
-	lw	$s2	12($sp)
-	lw	$a0	8($sp)
-	lw	$a1	4($sp)
 	lw	$ra	0($sp)
-	addu	$sp	$sp	24
+	addu	$sp	$sp	4
 	jr	$ra
 
 # Function PrintInt
@@ -202,22 +162,20 @@ Fin__Loop_PrintTable:
 # Returns:
 PrintInt:
 #Prologue:
-	subu	$sp	$sp	8
-	sw	$a0	4($sp)
-	sw	$ra	0($sp)
+	subu	$sp	$sp	4
+	sw	$a0	0($sp)
 
 #Body:
 	li	$v0	1
 	syscall
-
-	la	$a0	RetChar
+	
+	la	$a0	NewLine
 	li	$v0	4
 	syscall
 
 #Epilogue:
-	lw	$a0	4($sp)
-	lw	$ra	0($sp)
-	addu	$sp	$sp	8
+	lw	$a0	0($sp)
+	addu	$sp	$sp	4
 	jr	$ra
 
 __JR:
